ab2f61d3bdb9a97045149b43ae881b8f097883f0
EAX-PC <x3010@outlook.com>
2025-02-16 02:17:18 +0200

    #

--- src/Services/Ordering/Ordering.Application/Mappers/Mapper.cs
+++ src/Services/Ordering/Ordering.Application/Mappers/Mapper.cs
@@ -11,35 +11,29 @@ namespace Ordering.Application.Mappers
                 OrderName.Of(dto.OrderName),
                 dto.ShippingAddress.ToAddress(),
                 dto.BillingAddress.ToAddress(),
-                dto.Payment.ToPayment()
-            );
+                dto.Payment.ToPayment());
 
             foreach (var item in dto.OrderItems)
             {
                 order.Add(ProductId.Of(item.ProductId), item.Quantity, item.Price);
             }
-
             return order;
         }
-
-        public static Address ToAddress(this AddressDto dto)
-            => Address.Of(
+        public static Address ToAddress(this AddressDto dto) => Address.Of(
                 dto.FirstName,
                 dto.LastName,
                 dto.EmailAddress,
                 dto.AddressLine,
                 dto.State,
                 dto.Country,
-                dto.ZipCode
-            );
+                dto.ZipCode);
 
-        public static Payment ToPayment(this PaymentDto dto)
-            => Payment.Of(
+        public static Payment ToPayment(this PaymentDto dto) =>
+             Payment.Of(
                 dto.CardNumber,
                 dto.CardHolderName,
                 dto.CardExpiration,
                 dto.CardSecurityNumber,
-                1
-            );
+                1);
     }
 }
