bcecadaa600c9f80b33129f485e21ebc0e660b9d
EAX-PC <x3010@outlook.com>
2025-02-20 03:04:34 +0200

    #

--- src/Services/Ordering/Ordering.Application/Orders/Commands/UpdateOrder/UpdateOrderHandler.cs
+++ src/Services/Ordering/Ordering.Application/Orders/Commands/UpdateOrder/UpdateOrderHandler.cs
@@ -16,6 +16,7 @@ namespace Ordering.Application.Orders.Commands.UpdateOrder
 
             var DbOrder = await dbContext.Orders.FindAsync(orderId, cancellationToken);
             if (DbOrder == null) throw new OrderNotFoundException(orderId.Value);
+
             DbOrder.Update(order.OrderName, order.ShippingAddress, order.BillingAddress, order.Payment, order.Status);
             dbContext.Orders.Update(DbOrder);
             await dbContext.SaveChangesAsync(cancellationToken);
