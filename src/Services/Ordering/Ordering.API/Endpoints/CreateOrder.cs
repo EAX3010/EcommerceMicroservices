bc9f191376b3bc432d94fd8ca5da70af5444030a
EAX-PC <x3010@outlook.com>
2025-02-24 20:00:51 +0200

    Add Carter library and create order endpoint
    Introduce the Carter library to the Ordering API project for enhanced routing capabilities. Update `DependencyInjection.cs` to include Carter services. Add a new package reference for Carter version 9.0.0 in `Ordering.API.csproj`.
    
    Create `CreateOrder.cs` to define a new endpoint for order creation, utilizing MediatR for command handling and response mapping.
    
    Add Carter library and create order endpoint
    
    Introduce the Carter library to enhance routing in the Ordering API. Update `DependencyInjection.cs` to include `AddCarter` and `MapCarter`. Modify `Ordering.API.csproj` to add a package reference for Carter version 9.0.0. Add `CreateOrder.cs` to define a new endpoint for creating orders using the Carter framework.

---
+++ src/Services/Ordering/Ordering.API/Endpoints/CreateOrder.cs
@@ -0,0 +1,32 @@
+﻿using Carter;
+using Mapster;
+using MediatR;
+using Ordering.Application.Dtos;
+using Ordering.Application.Orders.Commands.CreateOrder;
+
+namespace Ordering.API.Endpoints
+{
+    public record CreateOrderRequest(OrderDto OrderDto);
+    public record CreateOrderResponse(Guid OrderId);
+    public class CreateOrder : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            Func<CreateOrderRequest, ISender, Task<IResult>> Handler = async (request, sender) =>
+            {
+                CreateOrderCommand command = request.Adapt<CreateOrderCommand>();
+                CreateOrderResult result = await sender.Send(command);
+                CreateOrderResponse response = result.Adapt<CreateOrderResponse>();
+                return Results.Created($"/orders/{response.OrderId}", response);
+            };
+            _ = app.MapPost("/orders", Handler);
+            //async Task<IResult> Handle(CreateOrderRequest request, ISender sender)
+            //{
+            //    CreateOrderCommand command = request.Adapt<CreateOrderCommand>();
+            //    CreateOrderResult result = await sender.Send(command);
+            //    CreateOrderResponse response = result.Adapt<CreateOrderResponse>();
+            //    return Results.Created($"/orders/{response.OrderId}", response);
+            //}
+        }
+    }
+}
