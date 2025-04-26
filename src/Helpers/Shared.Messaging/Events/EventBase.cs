32bdf77f419f9b7d85f331bd0b0cc3925f9212d5
EAX-PC <x3010@outlook.com>
2025-04-26 13:28:24 +0300

    Update project dependencies and add messaging events
    Updated project files to include new dependencies and version upgrades, notably adding MassTransit to `Shared.Messaging.csproj` and upgrading various packages in `Shared.csproj`, `Basket.API.csproj`, `Catalog.API.csproj`, `Discount.gRPC.csproj`, `Ordering.API.csproj`, `Ordering.Application.csproj`, `Ordering.Domain.csproj`, and `Ordering.Infrastructure.csproj`.
    
    Introduced new classes in the `Shared.Messaging.Events` namespace: `BasketCheckoutEvent` for checkout details and `EventBase` as a base class for events, encapsulating common properties.

---
+++ src/Helpers/Shared.Messaging/Events/EventBase.cs
@@ -0,0 +1,10 @@
+﻿namespace Shared.Messaging.Events
+{
+    public record EventBase
+    {
+        public Guid Id { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public string EventType => GetType().AssemblyQualifiedName;
+
+    }
+}
---
+++ src/Helpers/Shared.Messaging/MassTransit/Extensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Shared.Messaging.MassTransit
+{
+    public static class Extensions
+    {
+        public static IServiceCollection AddMessageBroker(this IServiceCollection services, Assembly? assembly = null)
+        {
+            return services;
+        }
+    }
+}
